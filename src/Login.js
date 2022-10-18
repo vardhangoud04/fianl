f5a4f3386c89fd8c5187c358da957c1a80901960
Vardhan Goud <vardhangoud41@gmail.com>
2022-10-18 17:40:09 +0530

    added new Login page

---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      <h2>Hello User</h2>
+      <h4>you are logged into the page successfully</h4>
+    </div>
+  )
+}
+
+export default Login
